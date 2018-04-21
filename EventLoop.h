d23244b1f4f0eb618b03a3d0c50c16b02512f890
joeylee97 <joeyleeeeeee97@github.com>
2018-04-21 12:07:27 +0800

    update

--- EventLoop.h
+++ EventLoop.h
@@ -1,10 +1,33 @@
 #pragma once
 #ifndef _EVENTLOOP_H
 #define _EVENTLOOP_H
+#include "nocopyable.h"
+#include <assert.h>
+#include "ThreadFunc.h"
+#include <pthread.h>
+namespace netlib {
+	class EventLoop : nocopyable {
+	public:
+		EventLoop();
+		~EventLoop();
+
+		void loop();
+
+		void assertInLoopThread() {
+			assert(isInLoopThread());
+		}
+
+		bool isInLoopThread() {
+			return _tid == curThreadId();
+		}
+	private:
+		ThreadId _tid;
+		bool looping;
+
+	};
+}
 
 
-
-class 
 
 
 #endif
